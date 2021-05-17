2681af511501dbd50249a41ecb2fc62a0a0fc62d
mensha <msksalston@gmail.com>
2021-05-17 17:33:30 -0400

    commit tree 1c3c010755d688916756df4d894de4478654d68f

--- index.js
+++ index.js
@@ -2,11 +2,24 @@
 
 const employee = {name: 'Sam', address: '295 Bainbridge street'}
 
-function updateEmployeeWithKeyAndValue(object, key, value) {
+function updateEmployeeWithKeyAndValue(employee, key, value) {
     const newEmployee = {...employee};
-   newEmployee[key] = '11 Broadway'
+     newEmployee[key] = value
         return newEmployee;
 }
- 
 
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+ function  deleteFromEmployeeByKey(employee, key) {
+      let employee2 = Object.assign({}, employee);
+        delete employee2[key]
+         return employee2 
+ }
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
 
