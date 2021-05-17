1f97f6e866d24d04cb8c752371dcb4c47e938c1b
mensha <msksalston@gmail.com>
2021-05-17 11:56:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+
+const employee = {name: 'Sam', address: '295 Bainbridge street'}
+
+function updateEmployeeWithKeyAndValue(employee, address, 11 Broadway) {
+    const newEmployee = {...employee};
+   newEmployee[address] = '11 Broadway'
+        return newEmployee;
+}
+ 
+
+
